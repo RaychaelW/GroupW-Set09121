9515c7884a8411286df808fd8d0ac7a79d4de082
RaychaelW <40671817@live.napier.ac.uk>
2025-11-17 14:22:05 +0000

    Add initial game structure with SFML: main.cpp, game.cpp, game.hpp, CMakeLists

---
+++ game.cpp
@@ -0,0 +1,16 @@
+#include "game.hpp"
+
+Game::Game() : window(sf::VideoMode(800, 600), "Game Window") {}
+
+void Game::run() {
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+}
